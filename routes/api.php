ceecc63f299aa56ab5b8a3a6ac28095da26051d2
v.plutahin <v.plutahin@zebrains.team>
2021-08-21 15:01:51 +0400

    docker config edits

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/forum', function (Request $request) {
+    return response('Test API', 200)
+        ->header('Content-Type', 'application/json');
+});
